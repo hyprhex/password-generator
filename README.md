f134466ecb9c56cfa0b5c8466e4dc92174404a5d
AIpill <swapapis@gmail.com>
2023-09-05 19:15:07 +0100

    Take user input and check if it's only number and more than 6

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Create a Python script that generate a random strong password from 3 types of ch
 ## Script steps:
 - [x] Import string module 
 - [x] Store all characters in lists 
-- [ ] Take number of characters from users 
-- [ ] Check if the user number equal or great than 6 
+- [x] Take number of characters from users 
+- [x] Check if the user number equal or great than 6 
 - [ ] Shuffle all lists 
 - [ ] Make sure password have 60% alphabets and 40% numbers and punctuations.
--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,3 +5,16 @@ upper_alphabet_character = list(string.ascii_uppercase)
 digit_character = list(string.digits)
 punctuation_character = list(string.punctuation)
 
+user_input = input("How many character you want for your password: ")
+
+while True:
+    try:
+        user_input = int(user_input)
+        if user_input < 6:
+            print("You need at least 6 characters")
+            user_input = input("Enter your password character: ")
+        else:
+            break
+    except:
+        print("Password character must be only numbers")
+        user_input = input("How many character you want for your password: ")
