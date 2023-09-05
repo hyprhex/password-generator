7c0914b841b2ea30cbcb1d07c9d728fb487e23d7
AIpill <swapapis@gmail.com>
2023-09-05 19:07:52 +0100

    Update

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -4,3 +4,4 @@ lower_alphabet_character = list(string.ascii_lowercase)
 upper_alphabet_character = list(string.ascii_uppercase)
 digit_character = list(string.digits)
 punctuation_character = list(string.punctuation)
+
